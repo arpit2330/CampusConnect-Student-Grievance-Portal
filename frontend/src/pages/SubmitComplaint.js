f2891bb7541321b7b72610f82d6de2dcb6a978db
Ayushu <rajawatayushu786@gmail.com>
2026-05-17 22:14:39 +0530

    fixed template string

--- frontend/src/pages/SubmitComplaint.js
+++ frontend/src/pages/SubmitComplaint.js
@@ -16,9 +16,9 @@ const SubmitComplaint = () => {
   try {
     const token = localStorage.getItem('token');
     await axios.post(
-      '${import.meta.env.VITE_API_URL}/api/complaints',
-      formData,
-      {
+   `${import.meta.env.VITE_API_URL}/api/complaints`,
+    formData,
+    {
         headers: {
           Authorization: `Bearer ${token}`,
         },
